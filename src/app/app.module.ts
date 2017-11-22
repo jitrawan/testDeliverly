38bb529b3306c0940813f09624e879817f7df0dd
DESKTOP-KAF1IKA\Aize <icetitiphong@gmail.com>
2017-11-22 11:57:25 +0700

    add footer and allevent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { AlleventComponent } from './eventTickets/allevent/allevent.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    AlleventComponent,
+    FooterComponent,
   ],
   imports: [
     NgbModule.forRoot(), BrowserModule,
---
+++ src/app/eventTickets/allevent/allevent.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-allevent',
+  templateUrl: './allevent.component.html',
+  styleUrls: ['./allevent.component.css',
+  '../../../assets/css/standard/cardticket.css',]
+})
+export class AlleventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

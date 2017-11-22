ec028aa4090bf0386c574c387c96b6c872799504
DESKTOP-KAF1IKA\Aize <icetitiphong@gmail.com>
2017-11-22 12:25:28 +0700

    fixed folder name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-import { AlleventComponent } from './eventTickets/allevent/allevent.component';
+
 import { FooterComponent } from './core/footer/footer.component';
+
+import { EventInfoComponent } from './event-info/event-info.component';
+import { AllEventComponent } from './eventTickets/all-event/all-event.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlleventComponent,
     FooterComponent,
+    EventInfoComponent,
+    AllEventComponent,
   ],
   imports: [
     NgbModule.forRoot(), BrowserModule,

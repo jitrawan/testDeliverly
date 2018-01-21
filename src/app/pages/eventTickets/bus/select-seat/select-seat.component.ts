bf8dea818809e8c17c64cefabb5406c930c396d7
SOMM <sasithornvie@7eleven.cp.co.th>
2018-01-21 21:08:10 +0700

    update page select seat

--- src/app/pages/eventTickets/bus/select-seat/select-seat.component.ts
+++ src/app/pages/eventTickets/bus/select-seat/select-seat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-select-seat',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectSeatComponent implements OnInit {
 
+  @Input() tripName: string;
+  @Input() dptrPark: string;
+  @Input() arrvPark: string;
+  @Input() dptrDate: Date;
+  @Input() arrvDate: Date;
+
   constructor() { }
 
   ngOnInit() {
+    this.tripName = "เที่ยวไป";
   }
 
+
+  detailContent: any = {
+    emptySeat: "ที่นั่งว่าง กดเลือกที่นั่งตามต้องการ",
+    sellSeat: "ที่นั่งถูกสำรองแล้ว",
+    markSeat: "ที่นั่งที่เลือก"
+  };
 }

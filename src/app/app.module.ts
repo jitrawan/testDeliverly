74cbf198146b37a93827f16c56117996743f7c44
IceTitiphong <icetitiphong@gmail.com>
2018-02-22 18:03:30 +0700

    Add reserve-transithistory component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,6 +53,7 @@ import { SummaryComponent } from './pages/eventTickets/bus/summary/summary.compo
 import { SelectRoundComponent } from './pages/eventTickets/bus/select-round/select-round.component';
 import { BuyTicketComponent } from './pages/eventTickets/bus/buy-ticket/buy-ticket.component';
 import { BusReceiveComponent } from './pages/eventTickets/bus/bus-receive/bus-receive.component';
+import { ReserveTransithistoryComponent } from './pages/reserve-history/reserve-transithistory/reserve-transithistory.component';
 
 @NgModule({
   declarations: [
@@ -84,7 +85,9 @@ import { BusReceiveComponent } from './pages/eventTickets/bus/bus-receive/bus-re
 
     ThaiDatePipe,
 
-    BusReceiveComponent
+    BusReceiveComponent,
+
+    ReserveTransithistoryComponent
   ],
   imports: [
     NgbModule.forRoot(),

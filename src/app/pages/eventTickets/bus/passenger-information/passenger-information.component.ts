7d89ffe40f2ccdca5f32a6989b12a0316216900e
HABALL <burin.haball@mail.kmutt.ac.th>
2018-02-28 11:18:08 +0700

    [ Updated ] Remove focus when dialog is open

--- src/app/pages/eventTickets/bus/passenger-information/passenger-information.component.ts
+++ src/app/pages/eventTickets/bus/passenger-information/passenger-information.component.ts
@@ -68,6 +68,7 @@ export class PassengerInformationComponent implements OnInit {
     let charCode = (event.which) ? event.which : event.keyCode;
     if (charCode != 46 && charCode > 31
       && (charCode < 48 || charCode > 57)) {
+      event.currentTarget.blur();
       this.openDialog(this.errorMessage.pleaseSelect + 'ตัวเลข');
       return false;
     }

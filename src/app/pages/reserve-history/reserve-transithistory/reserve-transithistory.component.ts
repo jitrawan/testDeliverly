7cf6caf4740e496d405e4f8c48ea6faffff1c7c9
IceTitiphong <icetitiphong@gmail.com>
2018-03-06 15:40:13 +0700

    update reserve-transithistory css ts html

--- src/app/pages/reserve-history/reserve-transithistory/reserve-transithistory.component.ts
+++ src/app/pages/reserve-history/reserve-transithistory/reserve-transithistory.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { TransReservToModel } from '../../../../app/shared/models/payment/transReservTo.model';
+import { UserTOModel } from '../../../../app/shared/models/payment/userTO.model';
 
 @Component({
   selector: 'app-reserve-transithistory',
   templateUrl: './reserve-transithistory.component.html',
-  styleUrls: ['./reserve-transithistory.component.css']
+  styleUrls: ['./reserve-transithistory.component.css', '../../../../assets/css/standard/utility.css']
 })
 export class ReserveTransithistoryComponent implements OnInit {
 
-  constructor() { }
+  transReservTo: TransReservToModel;
+  userTO: UserTOModel;
+  foodDetail: string = '1';
+  transitFoodTicketAvail: string = 'มีบัตรอาหาร';
+  transDetailTurn: string = '';
+
+  priceAmt: number = 0;
+  csFee: number = 0;
+  endPriceStr: number = 0;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this.transReservTo = new TransReservToModel;
+    this.transReservTo.reserveId = '810018043400168704'
+    this.transReservTo.transId = '999801802220954085'
+    this.transReservTo.data3 = '00538416'
+
+    this.userTO = new UserTOModel;
+    this.userTO.firstname = 'burin';
+    this.userTO.lastname = 'sangwan';
+    this.userTO.email = 'burinsan@gosoft.co.th';
+    this.userTO.cardNumber = '1234567890987';
+
+
   }
 
 }

82d4dd172fd59ab2dd5401e2ed05892974bf7457
gody6707 <got_za017@hotmail.com>
2018-03-20 16:00:52 +0700

    god
    update login google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -67,7 +67,8 @@ import { AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider, LinkedIn
 const config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
-    provider: new GoogleLoginProvider('624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com')
+    provider: new GoogleLoginProvider('425750395938-dv11rvh40k70nk9qmc4ao8uh2ph1dcm3.apps.googleusercontent.com')
+    // 624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com
   },
   {
     id: FacebookLoginProvider.PROVIDER_ID,
